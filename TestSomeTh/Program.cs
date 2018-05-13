6f369a91e9784daaafee01b99dd09e7d166eaef9
fleetingold <1161851523@qq.com>
2018-05-13 22:43:01 +0800

    20180513AppDomain学习

--- TestSomeTh/Program.cs
+++ TestSomeTh/Program.cs
@@ -1,15 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TestSomeTh
+namespace ErrorHandlingApplication
 {
     class Program
     {
-        static void Main(string[] args)
+        class DivNumbers
         {
+            int result;
+            DivNumbers()
+            {
+                result = 0;
+            }
+            public void division(int num1, int num2)
+            {
+                try
+                {
+                    result = num1 / num2;
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine("Exception caught: {0}", e);
+                }
+                finally
+                {
+                    Console.WriteLine("Result: {0}", result);
+                }
+
+            }
+            static void ErrorHandlingMain(string[] args)
+            {
+                DivNumbers d = new DivNumbers();
+                d.division(25, 0);
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+namespace UserDefinedException
+{
+    class TestTemperature
+    {
+        static void UserDefinedMain(string[] args)
+        {
+            Temperature temp = new Temperature();
+            try
+            {
+                temp.showTemp();
+            }
+            catch (TempIsZeroException e)
+            {
+                Console.WriteLine("TempIsZeroException: {0}", e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+public class TempIsZeroException : ApplicationException
+{
+    public TempIsZeroException(string message) : base(message)
+    {
+    }
+}
+public class Temperature
+{
+    public int temperature = 0;
+    public void showTemp()
+    {
+        if (temperature == 0)
+        {
+            throw (new TempIsZeroException("Zero Temperature found"));
+        }
+        else
+        {
+            Console.WriteLine("Temperature: {0}", temperature);
+        }
+    }
+}
+
+namespace NullMinusEqual
+{
+    class Program
+    {
+        public static void NullMinusEqualMain(string[] args)
+        {
+            Temperature temp = new Temperature();
+            temp.temperature -= -400;
+            Console.WriteLine(temp.temperature);
+            Console.ReadKey();
         }
     }
 }
