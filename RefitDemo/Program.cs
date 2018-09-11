5245b530db8798d9db84f21367c3d1cb98acf04a
fleetingold <1161851523@qq.com>
2018-09-12 00:57:35 +0800

    refit-get-test

---
+++ RefitDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace RefitDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var testApi = RestService.For<ITestApi>("http://localhost:52044/");
+
+            var str = await testApi.GetTest();
+            Console.WriteLine("获得的返回string为：" + str);
+            Console.ReadKey();
+        }
+    }
+
+    interface ITestApi
+    {
+        [Get("/JobManager/Test1")]
+        Task<string> GetTest();
+    }
+}
