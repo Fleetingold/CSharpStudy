e5f3574e0d600de0dcebc310fe4e6acf6e71cffb
fleetingold <1161851523@qq.com>
2018-09-12 09:46:08 +0800

    refitdemo2

--- RefitDemo/Program.cs
+++ RefitDemo/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Refit;
 
 namespace RefitDemo
@@ -15,13 +17,63 @@ namespace RefitDemo
 
             var str = await testApi.GetTest();
             Console.WriteLine("获得的返回string为：" + str);
+
+            var foo = new Foo
+            {
+                Bar = "a Foo"
+            };
+            var str2 = await testApi.CreateFoo(foo);
+            Console.WriteLine("获得的返回string为：" + str2);
+
+            var str3 = await testApi.CreateFoo(new Foo
+            {
+                Bar = "a Foo from json"
+            });
+            Console.WriteLine("获得的返回string为：" + str3);
+
+            //通用泛型定义接口
+            var api = RestService.For<IReallyExcitingCrudApi<Foo, string>>("http://localhost:52044/");
+
+            Foo result = await api.Create(new Foo
+            {
+                Bar = "a 通用泛型定义接口 Foo "
+            });
+            Console.WriteLine("获得的返回result.Bar为：" + result.Bar);
             Console.ReadKey();
         }
     }
 
     interface ITestApi
     {
-        [Get("/JobManager/Test1")]
+        [Get("/JobManager/Test")]
         Task<string> GetTest();
+
+        [Post("/JobManager/Test1")]
+        Task<string> CreateFoo([Body] Foo foo);
+    }
+
+    class Foo
+    {
+        //[JsonProperty(PropertyName = "b")]
+        public string Bar { get; set; }
+    }
+
+    //通用泛型定义接口
+    public interface IReallyExcitingCrudApi<T, in TKey> where T : class
+    {
+        [Post("/JobManager/Test2")]
+        Task<T> Create([Body] T payload);
+
+        [Get("")]
+        Task<List<T>> ReadAll();
+
+        [Get("/{key}")]
+        Task<T> ReadOne(TKey key);
+
+        [Put("/{key}")]
+        Task Update(TKey key, [Body]T payload);
+
+        [Delete("/{key}")]
+        Task Delete(TKey key);
     }
 }
