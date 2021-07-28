08f86c68433960610e777d237a31556705955d97
fleetingold <1161851523@qq.com>
2021-07-28 20:23:17 +0800

    add AsyncStateMachineProgram

--- CSharpStudy/AsyncAwaitProgram.cs
+++ CSharpStudy/AsyncAwaitProgram.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,7 +12,7 @@ namespace CSharpStudy
 {
     class AsyncAwaitProgram
     {
-        public static void Main()
+        public static void AsyncAwaitMain()
         {
             Console.WriteLine("Hey David, How much is 98745 divided by 7?");
 
@@ -129,4 +131,75 @@ namespace CSharpStudy
             Console.WriteLine("全部完成！");
         }
     }
+
+    #region AsyncStateMachineProgram
+
+    // This class is used by the code below to discover method attributes.
+    public class TheClass
+    {
+        public async Task<int> AsyncMethod()
+        {
+            await Task.Delay(5);
+            return 1;
+        }
+
+        public int RegularMethod()
+        {
+            return 0;
+        }
+    }
+
+    public class AsyncStateMachineProgram
+    {
+        private static bool IsAsyncMethod(Type classType, string methodName)
+        {
+            // Obtain the method with the specified name.
+            MethodInfo method = classType.GetMethod(methodName);
+
+            Type attType = typeof(AsyncStateMachineAttribute);
+
+            // Obtain the custom attribute for the method.
+            // The value returned contains the StateMachineType property.
+            // Null is returned if the attribute is't present for the method.
+            var attrib = (AsyncStateMachineAttribute)method.GetCustomAttribute(attType);
+
+            #region MyCode
+            var attr = method.GetCustomAttribute(attType);
+
+            if (attr is AsyncStateMachineAttribute) 
+            {
+                return true;
+            }
+            #endregion
+
+            return attrib != null;
+        }
+
+        private static void ShowResult(Type classType, string methodName)
+        {
+            Console.Write(methodName + ": ".PadRight(16));
+
+            if (IsAsyncMethod(classType, methodName))
+                Console.WriteLine("Async method");
+            else
+                Console.WriteLine("Regular method");
+        }
+
+        static void Main(string[] args)
+        {
+            ShowResult(classType: typeof(TheClass), methodName: nameof(TheClass.AsyncMethod));
+            ShowResult(classType: typeof(TheClass), methodName: nameof(TheClass.RegularMethod));
+
+            // Note: The IteratorStateMachineAttribute applies to Visual Basic methods
+            // but not C# methods.
+
+            Console.ReadKey(true);
+
+            // Output:
+            //  AsyncMethod:    Async method
+            //  RegularMethod:  Regular method
+        }
+    }
+
+    #endregion
 }
