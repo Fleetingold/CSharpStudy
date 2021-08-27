1aefac3dacf1952f7df9d7a4b03a2fca1fac6e6c
fleetingold <1161851523@qq.com>
2021-08-27 15:31:40 +0800

    CSharpStudy add ImplementInterfaceEvents

---
+++ CSharpStudy/ImplementInterfaceEvents.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    public interface IDrawingObject
+    {
+        event EventHandler ShapeChanged;
+    }
+
+    public class MyEventArgs : EventArgs
+    {
+        // class members
+    }
+
+    public class DrawingShape : IDrawingObject
+    {
+        public event EventHandler ShapeChanged;
+
+        void ChangeShape()
+        {
+            // Do something here before the event...
+
+            OnShapeChanged(new MyEventArgs(/*arguments*/));
+
+            // or do something here after the event.
+        }
+
+        protected virtual void OnShapeChanged(MyEventArgs e)
+        {
+            ShapeChanged?.Invoke(this, e);
+        }
+    }
+}
