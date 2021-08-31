9671dfe9188af4aff414d0b3b58f03650d7c364f
fleetingold <1161851523@qq.com>
2021-08-31 16:02:08 +0800

    add SampleGenericDelegateProgram

---
+++ CSharpStudy/SampleGenericDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    public class SampleGenericDelegateProgram
+    {
+        // Type T is declared covariant by using the out keyword.
+        public delegate T SampleGenericDelegate<out T>();
+
+        public static void Test()
+        {
+            SampleGenericDelegate<String> dString = () => " ";
+
+            // You can assign the dObject delegate
+            // to the same lambda expression as dString delegate
+            // because of the variance support for
+            // matching method signatures with delegate types.
+            SampleGenericDelegate<Object> dObject = () => " ";
+
+            // The following statement generates a compiler error
+            // because the generic type T is not marked as covariant.
+            SampleGenericDelegate<Object> dObj = dString;
+        }
+    }
+}
