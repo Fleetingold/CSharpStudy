fa01042b593447f9ef8c268dd7d3fdab21d32fef
fleetingold <1161851523@qq.com>
2021-08-31 11:55:48 +0800

    add CovariantDemo

---
+++ CSharpStudy/CovariantDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    class Base
+    {
+        public static void PrintBases(IEnumerable<Base> bases)
+        {
+            foreach(Base b in bases)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+
+    class Derived : Base
+    {
+
+    }
+
+    public class CovariantDemo
+    {
+        public static void CovariantMain()
+        {
+            List<Derived> dlist = new List<Derived>();
+
+            Base.PrintBases(dlist);
+            IEnumerable<Base> bIEnum = dlist;
+        }
+    }
+}
