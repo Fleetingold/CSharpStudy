bd52c417da80ba3bdc9dda4371bc9083818cb498
fleetingold <1161851523@qq.com>
2022-04-25 11:37:58 +0800

    CSharpStudy add FluentSchedulerDemo

---
+++ FluentSchedulerDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+FluentScheduler.JobManager.Initialize();
+
+FluentScheduler.JobManager.AddJob(
+    () => Console.WriteLine("1 minutes just passed."),
+    s => s.ToRunEvery(1).Minutes()
+);
+
+Console.ReadKey();
