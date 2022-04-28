d99f94ebc907364c193f849e2745306dd157370e
fleetingold <1161851523@qq.com>
2022-04-28 17:15:17 +0800

    FlurlTest add method ParsingUrlTest

--- FlurlDemoTests/FlurlTest.cs
+++ FlurlDemoTests/FlurlTest.cs
@@ -16,5 +16,20 @@ namespace FlurlDemoTests
             var url = "http://www.mysite.com".SetQueryParam("x", new[] { 1, 2, 3 });
             Assert.AreEqual("http://www.mysite.com?x=1&x=2&x=3", url);
         }
+
+        [TestMethod()]
+        public void ParsingUrlTest()
+        {
+            var url = new Url("https://user:pass@www.mysite.com:1234/with/path?x=1&y=2#foo");
+            Assert.AreEqual("https", url.Scheme);
+            Assert.AreEqual("user:pass", url.UserInfo);
+            Assert.AreEqual("www.mysite.com", url.Host);
+            Assert.AreEqual(1234, url.Port);
+            Assert.AreEqual("user:pass@www.mysite.com:1234", url.Authority);
+            Assert.AreEqual("https://user:pass@www.mysite.com:1234", url.Root);
+            Assert.AreEqual("/with/path", url.Path);
+            Assert.AreEqual("x=1&y=2", url.Query);
+            Assert.AreEqual("foo", url.Fragment);
+        }
     }
 }
