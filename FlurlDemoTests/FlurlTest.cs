4e4f6bb611c693af5dba55311c2a681bd61d9fc1
fleetingold <1161851523@qq.com>
2022-04-28 15:18:46 +0800

    CSharpStudy add FlurlDemoTests

---
+++ FlurlDemoTests/FlurlTest.cs
@@ -0,0 +1,20 @@
+﻿using Flurl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlurlDemoTests
+{
+    public class FlurlTest
+    {
+        [TestMethod()]
+        public void SetQueryParamTest()
+        {
+            var url = "http://www.mysite.com".SetQueryParam("x", new[] { 1, 2, 3 });
+            Assert.AreEqual("http://www.mysite.com?x=1&x=2&x=3", url);
+        }
+    }
+}
---
+++ FlurlDemoTests/Models/PersonTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlurlDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlurlDemo.Models.Tests
+{
+    public class PersonTests
+    {
+        [TestMethod()]
+        public void PersonTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
