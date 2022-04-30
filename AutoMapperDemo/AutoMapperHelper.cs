51a68c7bbe27939d40d9d4851030554991430c69
fleetingold <1161851523@qq.com>
2022-04-30 11:31:44 +0800

    add AutoMapperHelper

---
+++ AutoMapperDemo/AutoMapperHelper.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapperDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperDemo
+{
+    internal class AutoMapperHelper
+    {
+        private readonly AutoMapper.IMapper _mapper;
+
+        public AutoMapperHelper()
+        {
+            var config = new AutoMapper.MapperConfiguration(cfg => cfg.CreateMap<Order, OrderDto>());
+
+            _mapper = config.CreateMapper();
+
+            //var mapper2 = new AutoMapper.Mapper(config);
+        }
+
+        internal OrderDto GetOrderDto(Order order)
+        {
+            OrderDto dto = _mapper.Map<OrderDto>(order);
+
+            if(dto != null)
+            {
+                return dto;
+            }
+
+            //AutoMapper also has non-generic versions of these methods, for those cases where you might not know the type at compile time.
+            var mapResult = _mapper.Map(order, typeof(Order), typeof(OrderDto));
+            if (mapResult is OrderDto dto2)
+            {
+                return dto2;
+            }
+
+            return new OrderDto();
+        }
+    }
+}
--- AutoMapperDemo/Program.cs
+++ AutoMapperDemo/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+AutoMapperDemo.AutoMapperHelper autoMapper = new AutoMapperDemo.AutoMapperHelper();
+
+AutoMapperDemo.Models.Order order = new AutoMapperDemo.Models.Order();
+
+var orderDto = autoMapper.GetOrderDto(order);
+
+Console.ReadKey();
