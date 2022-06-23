273b200632c0293349df2e7c8ec8f39c33406561
fleetingold <1161851523@qq.com>
2022-06-23 09:32:29 +0800

    add project CSharpElevenStudy

---
+++ CSharpElevenStudy/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharpElevenStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //M("Hello, CSharpEleven!");
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        //public static void M(string s)
+        //{
+        //    if (s is null)
+        //    {
+        //        throw new ArgumentNullException(nameof(s));
+        //    }
+        //    // Body of the method
+        //}
+
+        //public static void M(string s!!)
+        //{
+        //    // Body of the method
+        //}
+    }
+}
