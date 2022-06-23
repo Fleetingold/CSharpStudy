91dc6f94218aaf3417ad1bde775e55a95642a003
fleetingold <1161851523@qq.com>
2022-06-23 09:19:24 +0800

    Parameter Null Checking

---
+++ CSharpStudy/ParamNullChecking/ParamNullChecking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.ParamNullChecking
+{
+    internal class ParamNullChecking
+    {
+        public static void M(string s)
+        {
+            if(s is null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+            // Body of the method
+        }
+
+        //public static void M(string s!!)
+        //{
+        //    // Body of the method
+        //}
+    }
+}
